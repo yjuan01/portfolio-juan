7ce50860774e3dd25def3722855a42c07c9f1553
soares <juansoaress714@gmail.com>
2025-10-30 11:33:57 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,30 @@ function App() {
 
   return (
   <>
-  <h1 className={styles.titulo}>flamengo</h1>
+   <nav className={styles.meu}>
+    <a href="#s1">Home</a>
+    <a href="#s2">Cards</a>
+    <a href="#s3">Video</a>
+    <a href="#s4">Contato</a>
+   </nav>
+   <main>
+      <section>
+        <h2>sessao 1</h2>
+      </section>
+
+       <section>
+        <h2>sessao 2</h2>
+      </section>
+
+       <section>
+        <h2>sessao 3</h2>
+      </section>
+
+       <section>
+        <h2>sessao 4</h2>
+      </section>
+
+   </main>
   </>
   )
 }
