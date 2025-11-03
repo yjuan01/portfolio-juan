8f6763ca58727a122af3ca4c51a21b02009addf6
soares <juansoaress714@gmail.com>
2025-11-03 11:48:40 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import  styles from './App.module.css'
+import fotoPerfil from '../public/juan.jpg'
 
 function App() {
   
@@ -13,19 +14,25 @@ function App() {
     <a href="#s4">Contato</a>
    </nav>
    <main>
-      <section>
-        <h2>sessao 1</h2>
+      <section className={styles.s1} id='s1'>
+        <div className={styles.left}> 
+          <img className={styles.imgPerfil} src={fotoPerfil} alt="Foto perfil" />
+        </div>
+        <div className='right'>
+          <h2 className={styles.title}>Juan Soares</h2>
+          <p className={styles.paragraph}>Estudante, aluno de desenvolvimento de sistema FIEP e Administração (ADM).  </p>
+        </div>
       </section>
 
-       <section>
+       <section id='s2'>
         <h2>sessao 2</h2>
       </section>
 
-       <section>
+       <section id='s3'>
         <h2>sessao 3</h2>
       </section>
 
-       <section>
+       <section id='s4'>
         <h2>sessao 4</h2>
       </section>
 
